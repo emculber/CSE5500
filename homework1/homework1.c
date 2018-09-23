af6f388ddea21b310dd9ae744c81ec5a940552d0
Erik <eculberson12@gmail.com>
2018-09-23 17:23:19 -0400

    Loading in full file

--- homework1/homework1.c
+++ homework1/homework1.c
@@ -2,7 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct region {
+#define SIZE 1024
+
+typedef struct region {
   char id[100];
   char type[10];
   char function_name[100];
@@ -13,7 +15,7 @@ struct region {
   char end_cpu[20];
   char level[10];
   char nested[100];
-};
+} region;
 
 struct single {
   char operation_id[100];
@@ -35,15 +37,36 @@ int check_file(char id[100], FILE* lookup) {
     if(strcmp(id, read_single.associated_function_call_instance_id) == 0) {
       count++; 
     }
-    //return count;
   }
   return count;
 }
 
+int line_count(FILE* file) {
+  char buffer[SIZE + 1], lastchar = '\n';
+  size_t bytes;
+  int lines = 0;
+
+  while ((bytes = fread(buffer, 1, sizeof(buffer) - 1, file))) {
+    lastchar = buffer[bytes - 1];
+    for (char *c = buffer; (c = memchr(c, '\n', bytes - (c - buffer))); c++) {
+      lines++;
+    }
+  }
+  if (lastchar != '\n') {
+    lines++;  /* Count the last line even if it lacks a newline */
+  }
+
+  return lines;
+}
+
 void find_in_file(FILE* source, FILE* lookup) {
   char id[100];
-  struct region single_region_line;
-  while (fscanf(source,"%s %s %s %s %s %s %s %s %s %s ", 
+  region single_region_line;
+  int continue_loop = 0;
+  printf("Lines in file %d\n", line_count(source));
+  return;
+  while (continue_loop) {
+      fscanf(source,"%s %s %s %s %s %s %s %s %s %s ", 
         single_region_line.id,
         single_region_line.type,
         single_region_line.function_name,
@@ -53,7 +76,7 @@ void find_in_file(FILE* source, FILE* lookup) {
         single_region_line.start_cpu,
         single_region_line.end_cpu,
         single_region_line.level,
-        single_region_line.nested)==10) {
+        single_region_line.nested);
 
     if(single_region_line.id[0] <= '9' && single_region_line.id[0] >= '0') {
       printf("Startin loockup for: %s", single_region_line.id); 
@@ -65,27 +88,38 @@ void find_in_file(FILE* source, FILE* lookup) {
   }
 }
 
+region* load_data_region(FILE* region_file, int size) {
+  region* region_data = malloc(size * sizeof(*region_data));
+  char line[SIZE];
+  int i = 0;
+
+  rewind(region_file);
+  while (fgets(line,SIZE, region_file)) {
+    //printf("%s\n", line);
+    strcpy(region_data[i++].id, strtok(line, " "));
+  }
+  return 0;
+}
+
+void load_data_single(FILE* single_file) {
+
+}
+
 int main(int argc, char *argv[])
 {
   FILE* region_file = fopen("region.out", "r");
   FILE* read_single_file = fopen("read_single.out", "r");
-  if (read_single_file != NULL) {
+  FILE* write_single_file = fopen("write_single.out", "r");
+  if (read_single_file != NULL && write_single_file != NULL && region_file != NULL) {
     printf("file opened\n");
-    find_in_file(region_file, read_single_file);
-    /*
-
-    struct single read_single; 
-    struct single read_single_other; 
-
-    while (fscanf(read_single_file,"%d %e %s %d ", 
-          &read_single.operation_id, 
-          &read_single.timestamp, 
-          read_single.reference_address, 
-          &read_single.associated_function_call_instance_id)==4) {
-      printf("%s\n", read_single.reference_address); 
-    }
-    */
+    //load_data(region_file, read_single_file, write_single_file);
+    int region_file_count = line_count(region_file);
+    load_data_region(region_file, region_file_count);
+    //find_in_file(region_file, read_single_file);
   } else {
     printf("file failed to open\n");
   }
+  fclose(region_file);
+  fclose(read_single_file);
+  fclose(write_single_file);
 }
